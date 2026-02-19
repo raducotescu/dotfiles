f774030680d3de12f54b1af6284b1c87125e2cb5
Radu Cotescu <cotescu@adobe.com>
2026-02-19 15:26:55 +0100

    removed wallpaper config

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
@@ -5,6 +5,3 @@ set -eufo pipefail
 # Use a single shared set of Spaces across all connected displays.
 defaults write com.apple.spaces spans-displays -bool true && killall SystemUIServer
 
-# Set every desktop to the built-in "Pro Black" wallpaper.
-WALLPAPER="/System/Library/Desktop Pictures/Pro Black.madesktop"
-osascript -e 'tell application "System Events" to tell every desktop to set picture to POSIX file "'"$WALLPAPER"'"'
