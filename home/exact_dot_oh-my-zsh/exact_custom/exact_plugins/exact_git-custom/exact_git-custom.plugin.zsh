1e7e99ee4f9acd442f107de3d85046dd2f3f940a
Radu Cotescu <radu-likes-to-code@cotescu.com>
2026-02-23 13:53:12 +0100

    oh-my-zsh optimisations

--- home/exact_dot_oh-my-zsh/exact_custom/exact_plugins/exact_git-custom/exact_git-custom.plugin.zsh
+++
@@ -1,8 +0,0 @@
-function gitCleanLocalOnlyBranches() {
-    git status >> /dev/null 2>&1
-    if [[ $? -eq 0 ]]; then
-        git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
-    fi
-}
-
-alias gbc='gitCleanLocalOnlyBranches'
